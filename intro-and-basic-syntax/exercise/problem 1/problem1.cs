6787901b10247ebb07c784e8d3e1061d69582121
Tereza Biserina <tbiserina@gmail.com>
2018-03-20 21:40:05 +0200

    document the code and add description in README.MD

--- intro-and-basic-syntax/exercise/problem 1/problem1.cs
+++ intro-and-basic-syntax/exercise/problem 1/problem1.cs
@@ -10,11 +10,13 @@ namespace Problem_1.Debit_Card_Number
     {
         static void Main(string[] args)
         {
+			//We receive 4 integer from the console
             int a = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
             int c = int.Parse(Console.ReadLine());
             int d = int.Parse(Console.ReadLine());
 
+			//Print the integers on the console in 4-digit debit card format
             Console.WriteLine($"{a:D4} {b:D4} {c:D4} {d:D4}");
         }
     }
