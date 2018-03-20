a43cd94db9fe3e8c45a8c4366e978d613710a205
Tereza Biserina <tbiserina@gmail.com>
2018-03-20 20:50:53 +0200

    add solution for problem 2

---
+++ intro-and-basic-syntax/exercise/problem 1/Problem2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Problem2
+{
+    class Problem2
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = Math.Round(width * height, 2);
+
+            Console.WriteLine(String.Format("{0:0.00}", area));
+        }
+    }
+}
