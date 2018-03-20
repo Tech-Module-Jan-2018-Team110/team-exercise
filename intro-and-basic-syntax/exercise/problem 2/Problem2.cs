f8faafacd15289266e1e890d566a70060ba395ab
PepiBobanov <p2pi@yahoo.com>
2018-03-20 21:05:57 +0200

    added comments

--- intro-and-basic-syntax/exercise/problem 2/Problem2.cs
+++ intro-and-basic-syntax/exercise/problem 2/Problem2.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+// removed unnecesary libraryes
 
 namespace Problem2
 {
@@ -6,11 +7,15 @@ namespace Problem2
     {
         static void Main(string[] args)
         {
+			// use double to avoid float overflow
             double width = double.Parse(Console.ReadLine());
             double height = double.Parse(Console.ReadLine());
+
             double area = Math.Round(width * height, 2);
 
+			// moje i taka: Console.WriteLine($"{area:0.00}");
             Console.WriteLine(String.Format("{0:0.00}", area));
+			
         }
     }
 }
